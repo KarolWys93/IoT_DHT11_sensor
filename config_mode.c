cff29361347ef7f31aa774babd93f533e9cc99ff
Karol Wyskocki <karol.wyskocki@vp.pl>
2019-04-03 01:03:03 +0200

    changes that reduce memory usage

--- config_mode.c
+++ config_mode.c
@@ -15,6 +15,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static void sendOK(void){
+	sendLine("ok");
+}
+
+static void getSetting(char *buffer, uint16_t len){
+	sendLine(">");
+	readLine(buffer, len, 0);
+}
+
 void config_mode(void){
 	
 	//if button is release, return from config mode
@@ -30,31 +39,25 @@ void config_mode(void){
 	
 	sendLine("Config mode");
 	sendLine("-----------");
-	sendLine("");
 	
 	sendLine("SSID:");
 	getSSID(resultText, sizeof resultText / sizeof *resultText);
 	sendLine(resultText);
-	sendLine("");
-	
+
 	sendLine("Pass:");
 	sendLine("********");
-	sendLine("");
-	
+
 	sendLine("Host:");
 	getHost(resultText, sizeof resultText / sizeof *resultText);
 	sendLine(resultText);
-	sendLine("");
 
 	sprintf(resultText, "Port: %u", getPort());
 	sendLine(resultText);
-	sendLine("");
 
 	sendLine("Topic:");
 	getTopic(resultText, sizeof resultText / sizeof *resultText);
 	sendLine(resultText);
-	sendLine("");
-	
+
 	sprintf(resultText, "Period: %u", getPeriod());
 	sendLine(resultText);
 	
@@ -65,50 +68,44 @@ void config_mode(void){
 		 
 		 if (strcmp(recivedText, "SSID") == 0)
 		 {
-			 sendLine(">");
-			 readLine(recivedText, sizeof recivedText / sizeof *recivedText, 0);
+			 getSetting(recivedText, sizeof recivedText / sizeof *recivedText);
 			 setSSID(recivedText, sizeof recivedText / sizeof *recivedText);
-			 sendLine("ok");
+			 sendOK();
 		 }
 		 
 		 if (strcmp(recivedText, "Pass") == 0)
 		 {
-			 sendLine(">");
-			 readLine(recivedText, sizeof recivedText / sizeof *recivedText, 0);
+			 getSetting(recivedText, sizeof recivedText / sizeof *recivedText);
 			 setWiFiPassword(recivedText, sizeof recivedText / sizeof *recivedText);
-			 sendLine("ok");
+			 sendOK();
 		 }
 		 
 		 if (strcmp(recivedText, "Host") == 0)
 		 {
-			 sendLine(">");
-			 readLine(recivedText, sizeof recivedText / sizeof *recivedText, 0);
+			 getSetting(recivedText, sizeof recivedText / sizeof *recivedText);
 			 setHost(recivedText, sizeof recivedText / sizeof *recivedText);
-			 sendLine("ok");
+			 sendOK();
 		 }
 		 
 		 if (strcmp(recivedText, "Port") == 0)
 		 {
-			 sendLine(">");
-			 readLine(recivedText, sizeof recivedText / sizeof *recivedText, 0);
+			 getSetting(recivedText, sizeof recivedText / sizeof *recivedText);
 			 setPort(atoi(recivedText));
-			 sendLine("ok");
+			 sendOK();
 		 }
 		 
 		 if (strcmp(recivedText, "Topic") == 0)
 		 {
-			 sendLine(">");
-			 readLine(recivedText, sizeof recivedText / sizeof *recivedText, 0);
+			 getSetting(recivedText, sizeof recivedText / sizeof *recivedText);
 			 setTopic(recivedText, sizeof recivedText / sizeof *recivedText);
-			 sendLine("ok");
+			 sendOK();
 		 }
 		 
 		 if (strcmp(recivedText, "Period") == 0)
 		 {
-			 sendLine(">");
-			 readLine(recivedText, sizeof recivedText / sizeof *recivedText, 0);
+			 getSetting(recivedText, sizeof recivedText / sizeof *recivedText);
 			 setPeriod(atoi(recivedText));
-			 sendLine("ok");
+			 sendOK();
 		 }
 	 }
 }
